929cb2f77ed963f882cd09092c99eca0051ccf93
Yohann Leon <yohann@leon.re>
2018-06-30 04:14:47 +0200

    fix: bad timezone used in moment.js for the timetable

--- jobs/timetable.rb
+++ jobs/timetable.rb
@@ -13,18 +13,18 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     events = cal.events
 
     # select only current and upcoming events
-    now = Time.now.utc
-    events = events.select{ |e| e.dtend.to_time.utc > now }
+    now = Time.now
+    events = events.select{ |e| e.dtend.to_time > now }
 
     # sort by start time
-    events = events.sort{ |a, b| a.dtstart.to_time.utc <=> b.dtstart.to_time.utc }[0..4]
+    events = events.sort{ |a, b| a.dtstart.to_time <=> b.dtstart.to_time }[0..4]
 
     events = events.map do |e|
       {
         title: e.summary,
         location: e.location.lines.first,
-        start: e.dtstart.to_time.to_i - 3600,
-        end: e.dtend.to_time.to_i - 3600
+        start: e.dtstart.to_time.to_i,
+        end: e.dtend.to_time.to_i
       }
     end
 
--- widgets/timetable/timetable.coffee
+++ widgets/timetable/timetable.coffee
@@ -1,8 +1,8 @@
 Batman.mixin Batman.Filters,
 
   aMomentPlease: (event)->
-    start = moment.unix(event.start)
-    end = moment.unix(event.end)
+    start = moment.unix(event.start).utc()
+    end = moment.unix(event.end).utc()
     "#{start.format('ddd, HH:mm') + " > " + end.format('HH:mm');}"
 
 class Dashing.Timetable extends Dashing.Widget
