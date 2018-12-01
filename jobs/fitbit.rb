eeabdade451f531bf7043a11c9dcb14e45c50b7c
Yohann Leon <yohann@leon.re>
2018-12-01 08:53:20 +0100

    fitbit: longer sync time to avoid api limit exceed

--- jobs/fitbit.rb
+++ jobs/fitbit.rb
@@ -5,7 +5,7 @@ animate_views = true
 
 fitbit = Fitbit.new unit_system: unit_system, date_format: date_format
 
-SCHEDULER.every "5m", first_in: 0 do |job|
+SCHEDULER.every "15m", first_in: 0 do |job|
   if fitbit.errors?
     send_event "fitbit", { error: fitbit.error }
   else
